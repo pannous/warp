d7dc57187abee2405c743ec4386be064bcb06c09
info@pannous.com <info@pannous.com>
2025-12-30 11:55:46 +0100

    feat(minor): add get_key() and get_value() accessors for KeyValue nodes
    - Add get_key() -> Option<&str> to extract key from KeyValue
    - Add get_value() -> Option<&Node> to extract value from KeyValue
    - Update test to use new accessors: n.get_key().unwrap()
    - Add Node::Todo() helper for placeholder nodes

--- tests/node_test.rs
+++ tests/node_test.rs
@@ -1,34 +1,54 @@
+use wast::parser::parse;
 use wasp::*;
 use wasp::node::Node;
-use wasp::node::Node::*;
+// use wasp::node::Node::*;
 
 #[test]
 fn test_node(){
     // let n:Node = Node::new();
+    // eval("key=value");
     let n:Node = Node::keys("key", "value");
+    is!(n.get_key().unwrap(), "key");
+    is!(n.get_value().unwrap(), &Node::text("value"));
     // let n:Node = KeyValue("key".s(), Box::new(Text("value".s())));
     println!("{:?}", n );
 }
 
+fn eval(code: &str) -> Node {
+    Node::Todo(format!("Eval not implemented: {}", code))
+}
 
 #[test]
 fn test_node_list() {
     let n: Node = Node::ints(vec![1, 2, 3]);
     println!("{:?}", n);
-    assert_eq!(n[0], Node::Number(wasp::Number::Int(1)));
+    assert_eq!(n[0], 1);
 }
 
 
 #[test]
 fn test_node_equality(){
-    let n:Node = Node::int(1);
+    let n0:Node = Node::int(0);
+    let n1:Node = Node::int(1);
     let n2:Node = Node::int(2);
-    assert_eq!(n, 1);
+    let n3:Node = Node::float(2.0);
+    assert_eq!(n1, 1);
     assert_eq!(n2, 2);
-    assert_ne!(n, n2);
-    assert_ne!(n, 2);
+    assert_eq!(n3, 2);
+    assert_eq!(n3, 2.0);
+    assert_eq!(n1, true);
+    assert_eq!(n0, false);
+    assert_ne!(n1, n2);
+    assert_ne!(n1, 2);
 }
 
+
+#[test]
+fn test_node_data_eq() {
+    let n = Node::data(vec![1, 2, 3]);
+    let n2 = n.clone();
+    is!(n,n2)
+}
 
 // #[test]
 // fn test_node_box() {
