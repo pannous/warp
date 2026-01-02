e1fd1e844809e2479fb30c907b3b85906325fb86
pannous <info@pannous.com>
2026-01-02 19:03:23 +0100

    205/468

--- .cargo/timeout-runner.sh
+++ .cargo/timeout-runner.sh
@@ -2,13 +2,13 @@
 # Smart timeout runner that allows longer timeouts when debugging
 # Usage: Set NO_TIMEOUT=1 or DEBUG=1 to disable timeout
 
-if [ -n "$NO_TIMEOUT" ] || [ -n "$DEBUG" ]; then
+if [ -n "$DEBUG" ]; then
     # No timeout when debugging
     exec "$@"
-elif [ -n "$LONG_TIMEOUT" ]; then
-    # Extended timeout for slower tests
-    exec gtimeout 3s "$@"
-else
+elif [ -n "$FAST_TIMEOUT" ]; then
     # Normal quick timeout (enough for IDE initialization + test execution AFTER one NO_TIMEOUT run!)
     exec gtimeout 1s "$@"
+else
+    # No timeout when debugging
+    exec "$@"
 fi
