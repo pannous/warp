c088e65dbca95513da8e6af978a02dfa1918e855
pannous <info@pannous.com>
2026-01-02 23:53:46 +0100

    204/466 test pass in 5sec

--- src/type_kinds.rs
+++ src/type_kinds.rs
@@ -42,22 +42,3 @@ pub enum AstKind {
 	Identifier,
 }
 
-// Wasp ABI GC Node representation design:
-// This is a single struct that can represent any node type
-/*
-(type $node (struct
-  (field $name_ptr i32)
-  (field $name_len i32)
-  (field $tag i32)
-  (field $int_value i64)
-  (field $float_value f64)
-  (field $text_ptr i32)
-  (field $text_len i32)
-  (field $left (ref null $node))  // recursive reference
-  (field $right (ref null $node))
-  (field $meta (ref null $node))
- ))
- */
-
-// todo move node layout to wasp_abi.rs
-// any change to node layout must be reflected in wasm_gc_reader.rs wasp_abi.md ... todo ...
