721e174201ddcd16aa5ec719aac687bca511fabb
pannous <info@pannous.com>
2026-01-13 18:01:23 +0100

    chore: update formatting and enable string fetch tests
    - Set max_width=120 in rustfmt.toml
    - Fix indentation in test_globals.rs (tabs)
    - Enable string fetch tests in test_web.rs (now working)
    - Add todo comment for test_host.rs improvement

--- tests/test_globals.rs
+++ tests/test_globals.rs
@@ -4,28 +4,28 @@ use warp::is;
 
 #[test]
 fn test_global_int_simple() {
-    is!("global x=10; x+5", 15);
+	is!("global x=10; x+5", 15);
 }
 
 #[test]
 fn test_global_int_multiple() {
-    is!("global x=10; global y=20; x+y", 30);
+	is!("global x=10; global y=20; x+y", 30);
 }
 
 #[test]
 fn test_global_with_expression() {
-    is!("global x=3*4; x+1", 13);
+	is!("global x=3*4; x+1", 13);
 }
 
 #[test]
 fn test_global_float_with_pi() {
-    use std::f64::consts::PI;
-    is!("global x=1+π", 1.0 + PI);
-    is!("global x=1+π; x+2", 3.0 + PI);
+	use std::f64::consts::PI;
+	is!("global x=1+π", 1.0 + PI);
+	is!("global x=1+π; x+2", 3.0 + PI);
 }
 
 #[test]
 fn test_global_reassignment() {
-    // DONE global reassignment: second declaration reuses existing global
-    is!("global x=5; global x=10; x", 10);
+	// DONE global reassignment: second declaration reuses existing global
+	is!("global x=5; global x=10; x", 10);
 }
